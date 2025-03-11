f48becef5e7fe8f40dc03225260d01b304f0404a
dolidolih <gagakagaga1@gmail.com>
2025-03-11 23:36:38 +0900

    converted AssetManager and PageRenderer to Kotlin

--- app/src/main/java/party/qwer/iris/AssetManager.kt
+++ app/src/main/java/party/qwer/iris/AssetManager.kt
@@ -1,30 +1,27 @@
-package party.qwer.iris;
+package party.qwer.iris
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Paths;
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.nio.file.Paths
 
-public class AssetManager {
-    public static String readFile(String filename) throws IOException {
-        ClassLoader loader = AssetManager.class.getClassLoader();
-        if (loader == null) {
-            throw new RuntimeException("ClassLoader를 찾을 수 없습니다.");
-        }
-        String path = Paths.get("assets", filename).toString();
+object AssetManager {
+    @Throws(IOException::class)
+    fun readFile(filename: String?): String {
+        val loader = AssetManager::class.java.classLoader
+            ?: throw RuntimeException("ClassLoader를 찾을 수 없습니다.")
+        val path = Paths.get("assets", filename).toString()
 
-        try (InputStream stream = loader.getResource(path).openStream()) {
-            int count;
-            byte[] buffer = new byte[1024];
-            ByteArrayOutputStream byteStream = new ByteArrayOutputStream(stream.available());
+        loader.getResource(path).openStream().use { stream ->
+            var count: Int
+            val buffer = ByteArray(1024)
+            val byteStream = ByteArrayOutputStream(stream.available())
 
             while (true) {
-                count = stream.read(buffer);
-                if (count <= 0) break;
-                byteStream.write(buffer, 0, count);
+                count = stream.read(buffer)
+                if (count <= 0) break
+                byteStream.write(buffer, 0, count)
             }
-
-            return byteStream.toString();
+            return byteStream.toString()
         }
     }
 }
--- app/src/main/java/party/qwer/iris/PageRenderer.kt
+++ app/src/main/java/party/qwer/iris/PageRenderer.kt
@@ -1,16 +1,22 @@
 // ConfigPageDocumentProvider.java
-package party.qwer.iris;
+package party.qwer.iris
 
-import java.io.IOException;
+import party.qwer.iris.Configurable.botName
+import party.qwer.iris.Configurable.botSocketPort
+import party.qwer.iris.Configurable.dbPollingRate
+import party.qwer.iris.Configurable.messageSendRate
+import party.qwer.iris.Configurable.webServerEndpoint
+import java.io.IOException
 
-public class PageRenderer {
-    public static String renderDashboard() throws IOException {
-        String html = AssetManager.readFile("dashboard.html");
-        html = html.replace("CURRENT_WEB_ENDPOINT", Configurable.INSTANCE.getWebServerEndpoint());
-        html = html.replace("CURRENT_BOT_NAME", Configurable.INSTANCE.getBotName());
-        html = html.replace("CURRENT_DB_RATE", String.valueOf(Configurable.INSTANCE.getDbPollingRate()));
-        html = html.replace("CURRENT_SEND_RATE", String.valueOf(Configurable.INSTANCE.getMessageSendRate()));
-        html = html.replace("CURRENT_BOT_PORT", String.valueOf(Configurable.INSTANCE.getBotSocketPort()));
-        return html;
+object PageRenderer {
+    @Throws(IOException::class)
+    fun renderDashboard(): String {
+        var html = AssetManager.readFile("dashboard.html")
+        html = html.replace("CURRENT_WEB_ENDPOINT", webServerEndpoint)
+        html = html.replace("CURRENT_BOT_NAME", botName)
+        html = html.replace("CURRENT_DB_RATE", dbPollingRate.toString())
+        html = html.replace("CURRENT_SEND_RATE", messageSendRate.toString())
+        html = html.replace("CURRENT_BOT_PORT", botSocketPort.toString())
+        return html
     }
 }
