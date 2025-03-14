1a4cff48bbae987d3a1447a00e007b36e2280a8f
dolidolih <gagakagaga1@gmail.com>
2025-03-15 01:06:56 +0900

    removed default http webserver value

--- app/src/main/java/party/qwer/iris/model/ConfigValues.kt
+++ app/src/main/java/party/qwer/iris/model/ConfigValues.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 data class ConfigValues(
     var botName: String = "Iris",
     var botHttpPort: Int = 3000,
-    var webServerEndpoint: String = "http://172.17.0.1:5000/db",
+    var webServerEndpoint: String = "",
     var dbPollingRate: Long = 100,
     var messageSendRate: Long = 50,
     var botId: Long = 0L
