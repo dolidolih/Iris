e318edbc4b71fe11f24fe3cddb44d435222b38f6
dolidolih <gagakagaga1@gmail.com>
2025-07-20 15:51:26 +0900

    - added print which db(mirrored or not) is selected - modified iris_control to choose adb connection when there are multiple devices connected. - created iris_control_termux to run iris without adb on the rooted phone.

--- app/src/main/java/party/qwer/iris/PathUtils.kt
+++ app/src/main/java/party/qwer/iris/PathUtils.kt
@@ -7,10 +7,10 @@ object PathUtils {
         val androidUid = System.getenv("KAKAOTALK_APP_UID") ?: "0"
         val mirrorPath = "/data_mirror/data_ce/null/$androidUid/com.kakao.talk/"
         val defaultPath = "/data/data/com.kakao.talk/"
-        
+
         return when {
-            File(mirrorPath).exists() -> mirrorPath
-            File(defaultPath).exists() -> defaultPath
+            File(mirrorPath).exists() -> mirrorPath.also { println("Returning mirrorPath: $it") }
+            File(defaultPath).exists() -> defaultPath.also { println("Returning defaultPath: $it") }
             else -> throw RuntimeException("KakaoTalk app path not found")
         }
     }
